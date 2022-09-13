a83a22d877534a1f8b4c6196dbe00611e8dfec17
Han Byul <yahb9@naver.com>
2022-09-13 17:41:15 +0900

    Feat: message type icon 추가

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,10 +1,19 @@
 import { Dispatch } from 'react'
 
+import { IMessage } from 'types/messageType'
+
+import { ErrorIcon, NoticeIcon, WarningIcon, XIcon } from 'assets/svgs'
 import styles from './modal.module.scss'
 
 interface IModalProps {
-  message: string
+  message: IMessage
   setOpenModal: Dispatch<React.SetStateAction<boolean>>
+}
+
+const modalIcon = {
+  ERROR: <ErrorIcon className={styles.typeIcon} />,
+  NOTIFICATION: <NoticeIcon className={styles.typeIcon} />,
+  WARNING: <WarningIcon className={styles.typeIcon} />,
 }
 
 const Modal = ({ message, setOpenModal }: IModalProps) => {
@@ -13,13 +22,20 @@ const Modal = ({ message, setOpenModal }: IModalProps) => {
   }
 
   return (
-    <div className={styles.modalBox}>
-      {}
-      <p>{message}</p>
-      <button type='button' onClick={handleModalButtonClick}>
-        확인
-      </button>
-    </div>
+    <>
+      <div className={styles.background} />
+      <div className={styles.modalBox}>
+        <XIcon className={styles.xIcon} onClick={handleModalButtonClick} />
+        <div className={styles.typeBox}>
+          {modalIcon.ERROR}
+          {message.kind.toUpperCase()}
+        </div>
+        <p>{message.message}</p>
+        <button type='button' onClick={handleModalButtonClick}>
+          확인
+        </button>
+      </div>
+    </>
   )
 }
 
