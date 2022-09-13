1d523453783e25335953ef8e6581439d7480e03e
Han Byul <yahb9@naver.com>
2022-09-13 16:30:31 +0900

    Feat: Geolocation api 추가 및 오류 시 모달 띄우기

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,17 +1,46 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import kakaoMap from 'utils/kakaoMap'
+import useMessages from 'hooks/useMessages'
+import Modal from 'components/Modal'
+import ModalPortal from 'components/Modal/ModalPortal'
 
 import styles from './main.module.scss'
 
 const Main = () => {
-  useEffect(() => {
-    kakaoMap()
-  }, [])
+  const [openModal, setOpenModal] = useState(false)
+  const { messages, message } = useMessages()
 
-  return <div id='kakaoMap' className={styles.mainMap} />
+  const retrieveSuccess = (position: any) => {
+    kakaoMap(position.coords.latitude, position.coords.longitude)
+  }
+
+  const retrieveError = useCallback(() => {
+    setOpenModal(true)
+    messages.error.geolocation.retrieveError()
+  }, [messages.error.geolocation])
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      setOpenModal(true)
+      messages.error.geolocation.notSuppertedError()
+    } else navigator.geolocation.watchPosition(retrieveSuccess, retrieveError)
+  }, [messages.error.geolocation, retrieveError])
+
+  return (
+    <>
+      <div id='kakaoMap' className={styles.mainMap} />
+      {openModal && (
+        <ModalPortal>
+          <Modal message={message} setOpenModal={setOpenModal} />
+        </ModalPortal>
+      )}
+    </>
+  )
 }
 
 export default Main
 
 // id를 ref로 바꿀 수 있음
+// declare 정확히 알기
+// env 파일에 key 집어넣기
