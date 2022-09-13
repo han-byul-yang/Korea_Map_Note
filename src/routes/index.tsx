b5cb3f2b4231ef21b186588c90acd78f2a0aff4c
Han Byul <yahb9@naver.com>
2022-09-13 20:47:23 +0900

    Feat: login 여부에 따라 컴포넌트 달리 보여주기 route 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { useSetRecoilState } from 'recoil'
+import { onAuthStateChanged } from 'firebase/auth'
 
+import { firebaseAuthService } from 'utils/firebaseSetting'
+import { userIdAtom } from 'store/atom'
 import Main from './Main'
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<Main />} />
-    </Routes>
-  )
+  const [isLoggin, setIsLoggin] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const setUserId = useSetRecoilState(userIdAtom)
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuthService, (user) => {
+      setIsLoading(true)
+      if (user) {
+        setIsLoggin(true)
+        setUserId(user.uid)
+      } else {
+        setIsLoggin(false)
+      }
+      setIsLoading(false)
+    })
+  })
+
+  return <Routes>{isLoggin ? <Route path='/' element={<Main />} /> : <Route path='/' element={<Auth />} />}</Routes>
 }
 
 export default App
