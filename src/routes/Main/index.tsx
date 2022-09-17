34c4b3689bb3019da39ac57f4a514e26fa348a0e
Han Byul <yahb9@naver.com>
2022-09-17 23:41:47 +0900

    Fix: react 형식으로 kakao map api 수정

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,21 +1,29 @@
 import { useEffect, useState } from 'react'
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
 
-import kakaoMap, { kakaoMapMark } from 'utils/kakaoMap'
 import modalMessage from 'utils/modalMessage'
 import { IMessage } from 'types/messageType'
 import { IGeolocationError, IGeolocationPosition } from 'types/geolocationType'
 import Modal from 'components/Modal'
+import InfoWindow from './InfoWindow'
 import ModalPortal from 'components/Modal/ModalPortal'
 
+import markImg from 'assets/img/mark.png'
 import styles from './main.module.scss'
 
 const Main = () => {
+  const [geolocation, setGeolocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  })
+  const [markPosition, setMarkPosition] = useState({ latitude: 0, longitude: 0 })
+  const [openInfoWindow, setOpenInfoWindow] = useState(false)
   const [openModal, setOpenModal] = useState(false)
   const [message, setMessage] = useState<IMessage>({ kind: '', message: '' })
 
   const retrieveSuccess = (position: IGeolocationPosition) => {
-    const map = kakaoMap(position.coords.latitude, position.coords.longitude)
-    kakaoMapMark(map)
+    setGeolocation({ latitude: position.coords.latitude!, longitude: position.coords.longitude! })
+    setMarkPosition({ latitude: position.coords.latitude!, longitude: position.coords.longitude! })
   }
 
   const retrieveError = (error: IGeolocationError) => {
@@ -33,7 +41,44 @@ const Main = () => {
 
   return (
     <>
-      <div id='kakaoMap' className={styles.mainMap} />
+      <Map
+        center={{
+          lat: geolocation.latitude,
+          lng: geolocation.longitude,
+        }}
+        style={{
+          width: '100vw',
+          height: '100vh',
+        }}
+        level={12}
+        onClick={(_t, mouseEvent) =>
+          setMarkPosition({
+            latitude: mouseEvent.latLng.getLat(),
+            longitude: mouseEvent.latLng.getLng(),
+          })
+        }
+      >
+        <MapMarker
+          position={{ lat: markPosition.latitude, lng: markPosition.longitude }}
+          clickable
+          onClick={() => setOpenInfoWindow(true)}
+          image={{
+            src: markImg,
+            size: {
+              width: 74,
+              height: 79,
+            },
+            options: {
+              offset: {
+                x: 27,
+                y: 69,
+              },
+            },
+          }}
+        >
+          {openInfoWindow && <InfoWindow />}
+        </MapMarker>
+      </Map>
       {openModal && (
         <ModalPortal>
           <Modal message={message} setOpenModal={setOpenModal} />
@@ -45,7 +90,6 @@ const Main = () => {
 
 export default Main
 
-// id를 ref로 바꿀 수 있음
 // declare 정확히 알기
 // env 파일에 key 집어넣기
 // 지도 렌더링 시간 너무 오래걸림
