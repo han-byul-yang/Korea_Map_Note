c7e1e63cb1fb90bcf967d815d2470352a332e7ed
Han Byul <yahb9@naver.com>
2022-09-18 12:54:07 +0900

    Feat: routes css 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,8 @@ import { userIdAtom } from 'store/atom'
 import Main from './Main'
 import Auth from './Auth'
 
+import styles from './routes.module.scss'
+
 const App = () => {
   const [isLoggin, setIsLoggin] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
@@ -26,7 +28,11 @@ const App = () => {
     })
   })
 
-  return <Routes>{isLoggin ? <Route path='/' element={<Main />} /> : <Route path='/' element={<Auth />} />}</Routes>
+  return (
+    <div className={styles.page}>
+      <Routes>{isLoggin ? <Route path='/' element={<Main />} /> : <Route path='/' element={<Auth />} />}</Routes>
+    </div>
+  )
 }
 
 export default App
