f942b80efd0107c129fbd39d721f870af6fac175
Han Byul <yahb9@naver.com>
2022-09-18 14:07:30 +0900

    Fix: 노트 추가 버튼 클릭 시 AddNoteModal 띄워주기

--- src/routes/Main/InfoWindow/index.tsx
+++ src/routes/Main/InfoWindow/index.tsx
@@ -1,34 +1,45 @@
 import { Dispatch, useEffect, useRef } from 'react'
 
 import useClickOutside from 'hooks/useClickOutside'
+import useResize from 'hooks/useResize'
 
 import { NotebookIcon, WriteIcon } from 'assets/svgs'
 import styles from './infoWindow.module.scss'
 
 interface IInfoWindowProps {
   setOpenInfoWindow: Dispatch<React.SetStateAction<boolean>>
+  setOpenAddNoteModal: Dispatch<React.SetStateAction<boolean>>
 }
 
-const InfoWindow = ({ setOpenInfoWindow }: IInfoWindowProps) => {
+const InfoWindow = ({ setOpenInfoWindow, setOpenAddNoteModal }: IInfoWindowProps) => {
   const containerRef = useRef(null)
   const { outsideClickEvent } = useClickOutside(containerRef, setOpenInfoWindow)
+  const { size, isSize: isMobile } = useResize()
 
   useEffect(() => {
     outsideClickEvent()
-  }, [outsideClickEvent])
+    size.MOBILE.RESIZE()
+    size.MOBILE.SIZEEVENT()
+  }, [outsideClickEvent, size.MOBILE])
+
+  const handleAddNoteClick = () => {
+    if (isMobile) setOpenAddNoteModal(true)
+  }
+
+  const handleReadNoteClick = () => {}
 
   return (
     <>
       <div className={styles.background} />
       <div className={styles.infoWindowContainer} ref={containerRef}>
-        <div className={styles.box}>
+        <button className={styles.box} type='button' onClick={handleAddNoteClick}>
           <WriteIcon className={styles.icon} />
           추억 추가
-        </div>
-        <div className={styles.box}>
+        </button>
+        <button className={styles.box} type='button' onClick={handleReadNoteClick}>
           <NotebookIcon className={styles.icon} />
           추억 보기
-        </div>
+        </button>
       </div>
     </>
   )
