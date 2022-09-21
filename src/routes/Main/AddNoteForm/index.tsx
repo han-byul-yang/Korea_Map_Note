bb56c62ede90b7e2c5ed5a3e27712e6ad28d338b
Han Byul <yahb9@naver.com>
2022-09-21 23:45:04 +0900

    Feat: addNoteForm mobile, desktop 반응형 디자인

--- src/routes/Main/AddNoteForm/index.tsx
+++ src/routes/Main/AddNoteForm/index.tsx
@@ -1,24 +1,49 @@
-import { Dispatch } from 'react'
+import { Dispatch, useEffect } from 'react'
 
-import { XIcon } from 'assets/svgs'
+import useResize from 'hooks/useResize'
+
+import { LeftArrowIcon, RightArrowIcon, XIcon } from 'assets/svgs'
 import styles from './addNoteForm.module.scss'
 
 interface IAddNoteProps {
   setOpenAddNoteForm: Dispatch<React.SetStateAction<boolean>>
+  openAddNoteForm: boolean
 }
 
-const AddNoteModal = ({ setOpenAddNoteForm }: IAddNoteProps) => {
+const AddNoteForm = ({ setOpenAddNoteForm, openAddNoteForm }: IAddNoteProps) => {
+  const { size, isSize: isMobile } = useResize()
+
+  useEffect(() => {
+    size.MOBILE.RESIZE()
+    size.MOBILE.SIZEEVENT()
+  }, [size.MOBILE])
+
   const handleXButtonClick = () => {
     setOpenAddNoteForm(false)
   }
 
+  const handleCloseButtonClick = () => {
+    setOpenAddNoteForm((prev) => !prev)
+  }
+
   return (
-    <div className={styles.background}>
-      <div className={styles.container}>
-        <XIcon className={styles.xIcon} onClick={handleXButtonClick} />
+    <div className={openAddNoteForm ? styles.openContainer : styles.closeContainer}>
+      <div className={styles.addNoteBox}>
+        {isMobile && <XIcon className={styles.xIcon} onClick={handleXButtonClick} />}
+        <p>지금 </p>
+        <form />
       </div>
+      {!isMobile && (
+        <button className={styles.closeButton} type='button' onClick={handleCloseButtonClick}>
+          {openAddNoteForm ? (
+            <LeftArrowIcon className={styles.arrowIcon} />
+          ) : (
+            <RightArrowIcon className={styles.arrowIcon} />
+          )}
+        </button>
+      )}
     </div>
   )
 }
 
-export default AddNoteModal
+export default AddNoteForm
