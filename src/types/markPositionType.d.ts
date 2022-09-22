3ae44f04d2750385fe369e1cb30b7315ebdb43c4
Han Byul <yahb9@naver.com>
2022-09-22 16:20:12 +0900

    Feat: markPositionType 타입 생성

---
+++ src/types/markPositionType.d.ts
@@ -0,0 +1,22 @@
+interface IPosition {
+  latitude: number
+  longitude: number
+}
+
+interface IMarkPositionKey {
+  geolocation: IPosition
+  location: IPosition
+}
+
+export interface IMarkPosition extends IMarkKeyPosition {
+  [key: string]: IPosition
+}
+
+interface IOpenInfoWindowKey {
+  geolocation: boolean
+  location: boolean
+}
+
+export interface IOpenInfoWindow extends IOpenInfoWindowKey {
+  [key: string]: boolean
+}
