17b79ca1b9f61e4d85a4a1aa3aee1fc170274679
Han Byul <yahb9@naver.com>
2022-09-22 16:20:52 +0900

    Feat: 클릭한 markPosition 위치 저장하는 atom 생성

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -4,3 +4,8 @@ export const userIdAtom = atom({
   key: 'userId',
   default: '',
 })
+
+export const clickedMarkPositionAtom = atom({
+  key: 'clickedMarkPosition',
+  default: { latitude: 0, longitude: 0 },
+})
