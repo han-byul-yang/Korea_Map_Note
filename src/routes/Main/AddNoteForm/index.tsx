4705cbe0d878f1ab349982753c491da71dc4dab2
Han Byul <yahb9@naver.com>
2022-10-02 19:12:42 +0900

    Feat: firebaseStore에 memo 저장로직 생성

--- src/routes/Main/AddNoteForm/index.tsx
+++ src/routes/Main/AddNoteForm/index.tsx
@@ -1,9 +1,11 @@
-import { Dispatch, useEffect, useState } from 'react'
+import { Dispatch, FormEvent, MouseEventHandler, useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
+import { addDoc, collection, DocumentData } from 'firebase/firestore'
 
 import useResize from 'hooks/useResize'
 import presentDate from 'utils/presentDate'
-import { clickedMarkPositionAtom } from 'store/atom'
+import { firebaseDB } from 'utils/firebaseSetting'
+import { clickedMarkPositionAtom, userIdAtom } from 'store/atom'
 import Picture from './Picture'
 import HashTag from './HashTag'
 
@@ -16,7 +18,14 @@ interface IAddNoteProps {
 }
 
 const AddNoteForm = ({ setOpenAddNoteForm, openAddNoteForm }: IAddNoteProps) => {
-  const [picture, setPicture] = useState<Blob>()
+  const [memo, setMemo] = useState<IMemo>({
+    siteName: '',
+    travelDate: '',
+    text: '',
+    picture: '',
+    hashTag: [''],
+  }) // type 설정
+  const userId = useRecoilValue(userIdAtom)
   const clickedMarkPosition = useRecoilValue(clickedMarkPositionAtom)
   const { size, isSize: isMobile } = useResize()
 
@@ -33,6 +42,55 @@ const AddNoteForm = ({ setOpenAddNoteForm, openAddNoteForm }: IAddNoteProps) => 
     setOpenAddNoteForm((prev) => !prev)
   }
 
+  const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setMemo((prev) => {
+      if (e.currentTarget?.name === 'siteName') {
+        return { ...prev, siteName: e.currentTarget.value }
+      }
+      if (e.currentTarget?.name === 'text') {
+        return { ...prev, text: e.currentTarget.value }
+      }
+      return { ...prev, hashTag: ['여행', '힐링'] }
+    })
+  }
+
+  const handleMemoSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    addDoc(collection(firebaseDB, 'memoInfo'), {
+      writer: userId,
+      createAt: new Date(),
+      geolocation: {
+        latitude: clickedMarkPosition.latitude,
+        logitude: clickedMarkPosition.longitude,
+      },
+      memo: {
+        siteName: memo.siteName,
+        travelDate: memo.travelDate,
+        text: memo.text,
+        picture: memo.picture,
+        hashTag: memo.hashTag,
+      },
+    })
+  }
+
+  const handleMemoClick = () => {
+    addDoc(collection(firebaseDB, 'memoInfo'), {
+      writer: userId,
+      createAt: new Date(),
+      geolocation: {
+        latitude: clickedMarkPosition.latitude,
+        logitude: clickedMarkPosition.longitude,
+      },
+      memo: {
+        siteName: memo.siteName,
+        travelDate: memo.travelDate,
+        text: memo.text,
+        picture: memo.picture,
+        hashTag: memo.hashTag,
+      },
+    })
+  }
+
   return (
     <div className={openAddNoteForm ? styles.openContainer : styles.closeContainer}>
       <div className={styles.addNoteBox}>
@@ -42,19 +100,22 @@ const AddNoteForm = ({ setOpenAddNoteForm, openAddNoteForm }: IAddNoteProps) => 
           <br />
           경도: {clickedMarkPosition.latitude} / 위도: {clickedMarkPosition.longitude}
         </p>
-        <form>
+        <form onSubmit={handleMemoSubmit}>
           <label>
             이 장소의 이름은?
-            <input type='text' name='name' />
+            <input type='text' name='siteName' value={memo.siteName} onChange={handleInputChange} />
           </label>
           <p>
             방문 시기: {presentDate()}
             <button type='button'>날짜 변경</button>
           </p>
-          <input type='text' />
-          <Picture setPicture={setPicture} />
+          <input type='text' name='text' value={memo.text} onChange={handleInputChange} />
+          <Picture />
           <HashTag />
         </form>
+        <button type='button' onClick={handleMemoClick}>
+          메모 저장
+        </button>
       </div>
       {!isMobile && (
         <button className={styles.closeButton} type='button' onClick={handleCloseButtonClick}>
