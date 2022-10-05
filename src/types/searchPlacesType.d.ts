cbbae0a9d20d5180b26d0c254c82ca0ff411a7ff
Han Byul <yahb9@naver.com>
2022-10-05 22:21:10 +0900

    Feat: ISearchAddressResultInfo,ISearchPlaceNameResultInfo type 설정

--- src/types/searchPlacesType.d.ts
+++ src/types/searchPlacesType.d.ts
@@ -1,4 +1,4 @@
-export interface ISearchResultInfo {
+export interface ISearchPlacesResultInfo {
   address_name: string
   category_group_code: string | null
   category_group_name: string | null
@@ -11,4 +11,18 @@ export interface ISearchResultInfo {
   road_address_name: string | null
   x: string
   y: string
+}
+
+export interface ISearchAddressResultInfo {
+  address: kakao.maps.services.Address
+  road_address: kakao.maps.services.RoadAaddress | null
+}
+
+export interface ISearchPlaceNameResultInfo {
+  address_name: string
+  address_type: 'REGION' | 'ROAD' | 'REGION_ADDR' | 'ROAD_ADDR'
+  x: string
+  y: string
+  address: kakao.maps.services.Address
+  road_address: kakao.maps.services.RoadAaddress
 }
