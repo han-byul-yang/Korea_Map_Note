ab25534b45d99181802b47db8a719ab19a190683
Han Byul <yahb9@naver.com>
2022-10-08 02:22:51 +0900

    Feat: firebase에 저장하는 memoDocsType 설정

--- src/types/memoType.d.ts
+++ src/types/memoType.d.ts
@@ -1,7 +1,17 @@
-interface IMemo {
+export interface IMemo {
   siteName: string
   travelDate: string
   text: string
   picture: string | null
   hashTag: Array | null
+}
+
+export interface IMemoDocs {
+  createAt: Date
+  geolocation: {
+    latitude: number
+    longitude: number
+  }
+  memo: IMemo
+  writer: string
 }
