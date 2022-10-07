204c7da7918e6f379ee3c30f90e0b053e94910b1
Han Byul <yahb9@naver.com>
2022-10-08 02:25:21 +0900

    Feat: docs id로 firebase의 문서가져오는 getDocsFromFirebase util 생성

---
+++ src/utils/firebaseService/firebaseDBService.ts
@@ -0,0 +1,10 @@
+import { query, collection, getDocs } from 'firebase/firestore'
+
+import { firebaseDBService } from './firebaseSetting'
+
+export const getDocsFromFirebase = async (id: string) => {
+  const collectionQuery = query(collection(firebaseDBService, id))
+  const docs = await getDocs(collectionQuery)
+
+  return docs
+}
