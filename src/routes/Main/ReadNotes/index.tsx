a6511a9738f09993b7786e424944ceb6acde73da
Han Byul <yahb9@naver.com>
2022-10-09 18:10:19 +0900

    Feat: infoWindow 메모 보기 클릭 시 나오는 ReadNote component 생성

---
+++ src/routes/Main/ReadNotes/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import { useRecoilState, useSetRecoilState } from 'recoil'
+
+import useResize from 'hooks/useResize'
+import { isOpenReadNotesAtom, memoAtom } from 'store/atom'
+
+import { XIcon } from 'assets/svgs'
+import styles from './storedNotes.module.scss'
+
+const ReadNotes = () => {
+  const [openReadNotes, setOpenReadNotes] = useRecoilState(isOpenReadNotesAtom)
+  const setMemo = useSetRecoilState(memoAtom)
+  const { size, isSize: isMobile } = useResize()
+
+  useEffect(() => {
+    size.MOBILE.RESIZE()
+    size.MOBILE.SIZEEVENT()
+  }, [size.MOBILE])
+
+  const handleCloseButtonClick = () => {
+    setOpenReadNotes(false)
+    setMemo((prevMemo) => ({ ...prevMemo, siteName: '' }))
+  }
+
+  return (
+    <div className={openReadNotes ? styles.openContainer : styles.closeContainer}>
+      <div className={styles.readNotesBox} />
+      {!isMobile && openReadNotes && (
+        <button className={openReadNotes && styles.closeButton} type='button' onClick={handleCloseButtonClick}>
+          <XIcon className={styles.arrowIcon} />
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default ReadNotes
