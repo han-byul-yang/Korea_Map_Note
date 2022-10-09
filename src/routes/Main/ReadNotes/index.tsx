c34819b6c67451224c46c80374b20abacf624ca1
Han Byul <yahb9@naver.com>
2022-10-09 21:00:52 +0900

    Feat: firebase 에서 클릭한 mark와 같은 위치의 memo docs 가져오기

--- src/routes/Main/ReadNotes/index.tsx
+++ src/routes/Main/ReadNotes/index.tsx
@@ -1,21 +1,36 @@
 import { useEffect } from 'react'
-import { useRecoilState, useSetRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 
 import useResize from 'hooks/useResize'
-import { isOpenReadNotesAtom, memoAtom } from 'store/atom'
+import { isOpenReadNotesAtom, markPositionAtom, memoAtom, userIdAtom } from 'store/atom'
+import { getDocsFromFirebase } from 'utils/firebaseService/firebaseDBService'
 
 import { XIcon, HamburgerCloseIcon } from 'assets/svgs'
 import styles from './readNotes.module.scss'
 
 const ReadNotes = () => {
+  const userId = useRecoilValue(userIdAtom)
   const [openReadNotes, setOpenReadNotes] = useRecoilState(isOpenReadNotesAtom)
   const setMemo = useSetRecoilState(memoAtom)
+  const markPosition = useRecoilValue(markPositionAtom)
   const { size, isSize: isMobile } = useResize()
 
   useEffect(() => {
     size.MOBILE.RESIZE()
     size.MOBILE.SIZEEVENT()
   }, [size.MOBILE])
+
+  useEffect(() => {
+    getDocsFromFirebase(userId).then((memoDocs) => {
+      const result = memoDocs.docs
+        .map((firebaseMemo) => firebaseMemo.data().data)
+        .filter(
+          (doc) =>
+            doc.geolocation?.latitude === markPosition.clickedPosition.latitude &&
+            doc.geolocation?.longitude === markPosition.clickedPosition.longitude
+        )
+    })
+  }, [markPosition.clickedPosition.latitude, markPosition.clickedPosition.longitude, userId])
 
   const handleCloseButtonClick = () => {
     setOpenReadNotes(false)
