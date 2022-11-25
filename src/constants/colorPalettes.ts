295b7e5bc38678362aca6f278103a328c0500211
Han Byul <yahb9@naver.com>
2022-11-25 17:19:58 +0900

    Feat: hashTag color palette 생성

---
+++ src/constants/colorPalettes.ts
@@ -0,0 +1,1 @@
+export const colorPalettes = ['#f95959', '#ff9a3c', '#facf5a', '#42b883', '#a2a8d3']
--- src/routes/Main/AddNoteForm/HashTag/index.tsx
+++ src/routes/Main/AddNoteForm/HashTag/index.tsx
@@ -2,10 +2,18 @@ import { FormEvent, useState } from 'react'
 import { useRecoilState } from 'recoil'
 
 import { memoAtom } from 'store/atom'
+import { colorPalettes } from 'constants/colorPalettes'
+
+import styles from './hashTag.module.scss'
 
 const HashTag = () => {
+  const [selecedColor, setSelectedColor] = useState('f95959')
   const [hashTag, setHashTag] = useState('')
   const [memo, setMemo] = useRecoilState(memoAtom)
+
+  const handleColorChange = (e: FormEvent<HTMLInputElement>) => {
+    setSelectedColor(e.currentTarget.value)
+  }
 
   const handleHashTagChange = (e: FormEvent<HTMLInputElement>) => {
     setHashTag(e.currentTarget.value)
@@ -26,13 +34,30 @@ const HashTag = () => {
   }
 
   return (
-    <form onSubmit={handleHashTagSubmit}>
+    <form className={styles.hashTag} onSubmit={handleHashTagSubmit}>
       <p>해시태그</p>
+      {colorPalettes.map((color) => (
+        <>
+          <input
+            key={color}
+            type='radio'
+            name='palette'
+            value={color}
+            onChange={handleColorChange}
+            style={{ backgroundColor: `${color}` }}
+          />
+          <label htmlFor={color} />
+        </>
+      ))}
       <ul>
         {memo.hashTagList.map((tag: string) => {
           return (
             <li key={tag}>
-              <button type='button' onClick={() => handleHashTagClick(tag)}>
+              <button
+                type='button'
+                onClick={() => handleHashTagClick(tag)}
+                style={{ backgroundColor: `${selecedColor}` }}
+              >
                 {tag}
               </button>
             </li>
