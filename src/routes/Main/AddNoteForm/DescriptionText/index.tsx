59b657e9f13952bf026c3547c77226413d53307c
Han Byul <yahb9@naver.com>
2022-11-27 15:03:50 +0900

    Design: DescriptionText 디자인

--- src/routes/Main/AddNoteForm/DescriptionText/index.tsx
+++ src/routes/Main/AddNoteForm/DescriptionText/index.tsx
@@ -1,16 +1,20 @@
-import { FormEvent } from 'react'
+import { ChangeEvent } from 'react'
 import { useRecoilState } from 'recoil'
 
 import { memoAtom } from 'store/atom'
 
+import styles from './descriptionText.module.scss'
+
 const DescriptionText = () => {
   const [memo, setMemo] = useRecoilState(memoAtom) // type 설정
 
-  const handleTextChange = (e: FormEvent<HTMLInputElement>) => {
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setMemo((prevMemo) => ({ ...prevMemo, text: e.currentTarget.value }))
   }
 
-  return <input type='text' name='text' value={memo.text} onChange={handleTextChange} />
+  return (
+    <textarea className={styles.descriptionText} name='text' rows={100} value={memo.text} onChange={handleTextChange} />
+  )
 }
 
 export default DescriptionText
