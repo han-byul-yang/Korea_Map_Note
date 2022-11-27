0a7e40040b44766e40058c818e02b6f8cd7872f8
Han Byul <yahb9@naver.com>
2022-11-27 15:07:25 +0900

    Design: TravelDate 디자인

--- src/routes/Main/AddNoteForm/TravelDate/CustomDateButton.tsx
+++ src/routes/Main/AddNoteForm/TravelDate/CustomDateButton.tsx
@@ -1,11 +1,15 @@
 import { forwardRef, LegacyRef } from 'react'
 
+import { CalendarIcon } from 'assets/svgs'
+import styles from './travelDate.module.scss'
+
 export const CustomDateButton = forwardRef(({ value, onClick }: any, ref: LegacyRef<HTMLButtonElement> | undefined) => (
-  <div>
+  <>
+    <CalendarIcon className={styles.calendarIcon} />
     <button type='button' onClick={onClick} ref={ref}>
       <div>{value}</div>
     </button>
-  </div>
+  </>
 ))
 
 CustomDateButton.displayName = 'customButton'
--- src/routes/Main/AddNoteForm/TravelDate/index.tsx
+++ src/routes/Main/AddNoteForm/TravelDate/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
 import ReactDatePicker from 'react-datepicker'
 
@@ -5,22 +6,27 @@ import { memoAtom } from 'store/atom'
 import { CustomDateButton } from './CustomDateButton'
 
 import 'react-datepicker/dist/react-datepicker.css'
+import styles from './travelDate.module.scss'
+import dayjs from 'dayjs'
 
 const TravelDate = () => {
   const [memo, setMemo] = useRecoilState(memoAtom)
 
   const handleDateChange = (dates: [Date | null, Date | null]) => {
     const [start, end] = dates
-    setMemo((prevTask) => ({ ...prevTask, travelDate: { startDate: start, endDate: end } }))
+    setMemo((prevTask) => ({
+      ...prevTask,
+      travelDate: { startDate: start, endDate: end },
+    }))
   }
 
   return (
-    <div>
+    <div className={styles.customDateButton}>
       <ReactDatePicker
-        selected={memo.travelDate.startDate}
+        selected={new Date()}
         onChange={handleDateChange}
-        startDate={memo.travelDate.startDate}
-        endDate={memo.travelDate.endDate}
+        startDate={new Date()}
+        endDate={null}
         selectsRange
         dateFormat='yyyy.MM.dd'
         customInput={<CustomDateButton />}
