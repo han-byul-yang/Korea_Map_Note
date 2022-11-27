fe2d6de5983aadf83f4f0c4222f0962f486662fd
Han Byul <yahb9@naver.com>
2022-11-27 15:06:50 +0900

    Design: Address 디자인

--- src/routes/Main/AddNoteForm/Address/index.tsx
+++ src/routes/Main/AddNoteForm/Address/index.tsx
@@ -1,16 +1,22 @@
 import { ISearchAddressResultInfo } from 'types/searchPlacesType'
 
+import { LocationIcon } from 'assets/svgs'
+import styles from './address.module.scss'
+
 interface IAddressProps {
   addressResult: ISearchAddressResultInfo[] | undefined
 }
 
 const Address = ({ addressResult }: IAddressProps) => {
   return (
-    <p>
-      기본 주소: {addressResult && addressResult[0]?.address?.address_name}
-      <br />
-      도로명 주소: {addressResult && addressResult[0]?.road_address?.address_name}
-    </p>
+    <div className={styles.address}>
+      <LocationIcon className={styles.loactionIcon} />
+      <span className={styles.addressTitle}>주소</span>
+      <div className={styles.locationAddress}>
+        <p>기본 주소: {addressResult && addressResult[0]?.address?.address_name}</p>
+        <p>도로명 주소: {addressResult && addressResult[0]?.road_address?.address_name}</p>
+      </div>
+    </div>
   )
 }
 
